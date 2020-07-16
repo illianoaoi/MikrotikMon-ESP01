9774e6a56221f41bbb84636c79e513f0427aaf23
illianoaoi <leeyagart@gmail.com>
2020-07-16 20:21:33 +0700

    ทดสอบ

--- MikrotikMon-ESP01.ino
+++ MikrotikMon-ESP01.ino
@@ -15,6 +15,32 @@
 #define LED 2
 unsigned char status_RELAY = 0;
 
+BLYNK_WRITE(V7)
+{
+  int pinValue = param.asInt(); 
+  
+  if (pinValue == 1)
+  {
+    status_RELAY = 1;
+    digitalWrite(RELAY, HIGH);
+    Serial.println("RELAY ON");
+    WidgetLED led1(V5);
+    led1.on();
+    digitalWrite(LED, HIGH);
+    
+  }
+  else if (pinValue == 0)
+  {
+    status_RELAY = 0;
+    digitalWrite(RELAY, LOW);
+    Serial.println("RELAY OFF");
+    WidgetLED led1(V5);
+    led1.off();
+    digitalWrite(LED, LOW);
+  }
+  
+}
+
 
 void setup() {
   //Blynk เชื่อมต่อกับ Server
@@ -24,11 +50,14 @@ void setup() {
   String ip = connectWiFi();  // สั่งให้เชื่อมต่อกับ AP
   Blynk.virtualWrite(V0, ip); //ส่งไปที่ Blynk
 
-  ///////// Relay ////////////
-  pinMode(RELAY, OUTPUT);
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, HIGH);
-  ////////////////////////////
+    ///////// Relay ////////////
+    pinMode(RELAY, OUTPUT);
+    pinMode(LED, OUTPUT);
+    digitalWrite(LED, HIGH);
+    Blynk.virtualWrite(V7, 0);
+    WidgetLED led1(V5);
+    led1.off();
+    ////////////////////////////
   
   delay(5000);
 }
@@ -42,6 +71,31 @@ void loop() {
   Blynk.virtualWrite(V2, getCPU()); //ส่งไปที่ Blynk
   Blynk.virtualWrite(V3, getdownload()); //ส่งไปที่ Blynk
   Blynk.virtualWrite(V4, getupload()); //ส่งไปที่ Blynk
+
+  int Temp = getTemp().toInt();
+
+  if (Temp >= 33)
+  {
+    status_RELAY = 1;
+    digitalWrite(RELAY, HIGH);
+    Serial.println("RELAY ON");
+    WidgetLED led1(V5);
+    led1.on();
+    digitalWrite(LED, HIGH);
+    Blynk.virtualWrite(V7, 1);
+    
+  }
+  else if (Temp <= 32)
+  {
+    status_RELAY = 0;
+    digitalWrite(RELAY, LOW);
+    Serial.println("RELAY OFF");
+    WidgetLED led1(V5);
+    led1.off();
+    digitalWrite(LED, LOW);
+    Blynk.virtualWrite(V7, 0);
+    
+  }
   
   delay(Refesh);
 }
